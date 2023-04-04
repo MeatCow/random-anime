93e279f696dc5b86f7c6acbaf12db68bac219840
Matt <matt.pauze@gmail.com>
2023-04-03 21:50:13 -0400

    alright, lets use analog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [RouterOutlet],
+  template: ` <router-outlet></router-outlet> `,
+  styles: [
+    `
+      :host {
+        max-width: 1280px;
+        margin: 0 auto;
+        padding: 2rem;
+        text-align: center;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-  title = 'random-anime';
-}
+export class AppComponent {}
